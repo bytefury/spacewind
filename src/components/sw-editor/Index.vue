3de314f3f070f04ad1ac362b158a12d486b05c23
Mohit Panjwani <mohit.panjvani@gmail.com>
2020-11-28 14:16:55 +0530

    fix editor issue for auto-italic

--- src/components/sw-editor/Index.vue
+++ src/components/sw-editor/Index.vue
@@ -340,6 +340,8 @@ export default {
   methods: {
     initEditor() {
       this.editor = new Editor({
+        disableInputRules: true,
+        disablePasteRules: true,
         extensions: [
           new Blockquote(),
           new CodeBlock(),
